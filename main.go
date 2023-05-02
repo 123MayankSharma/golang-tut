d332342006454e594bffd31455f5eb6d00d3e065
123MayankSharma <mayanksharma922001@gmail.com>
2023-05-03 02:31:47 +0530

    Saved File

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	mp := map[string]int{}
 
-func main(){
-	fmt.Println("Hello")
+	mp["a"] = 1
+	mp["b"] = 2
+	mp["c"] = 3
+	//fill with all english characters
+	mp["d"] = 4
+	mp["e"] = 5
+	mp["f"] = 6
+
+	mapString := ""
+	for key, value := range mp {
+		mapString += fmt.Sprintf("%s:%d\n", key, value)
+	}
+	//converting string to  byte array
+	data := []byte(mapString)
+	os.WriteFile("TestFile.txt", data, 0644)
 }
