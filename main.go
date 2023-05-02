812369c866e92d33d5fb898579e2433062c5b566
123MayankSharma <mayanksharma922001@gmail.com>
2023-05-02 23:57:06 +0530

    .

--- main.go
+++ main.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+func changeString(s *string) {
+	//we can access value by using *
+	fmt.Println("Initial value of s is: ", *s)
+	*s = "Mayank"
+}
 
-func main(){
-	fmt.Println("Hello")
+func main() {
+	var s string = "Rahul"
+	//we pass by reference by using & operator while calling
+	//and variable *data_type while using
+	// we access pointer values by using * as illustrated above
+	changeString(&s)
+	fmt.Println(s)
 }
