fdf63be65bba5281c140a34e17821f169e401e84
123MayankSharma <mayanksharma922001@gmail.com>
2023-05-02 20:01:11 +0530

    .

--- main.go
+++ main.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+func main() {
+	//here string is key type and int is value type
+	mp := map[string]int{"Apple": 0}
 
-func main(){
-	fmt.Println("Hello")
+	//assigning values in map
+	mp["Mayank"] = 1
+	mp["sharma"] = 2
+
+	//printing value from a map
+	fmt.Println(mp["Apple"])
+
+	//looping over a map
+	for key, value := range mp {
+		fmt.Println(key, ": ", value)
+	}
+
 }
